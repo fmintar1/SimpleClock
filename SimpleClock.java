bbb66f6af8a751c228e8428b5ec63169d2d7490b
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-12-10 14:03:13 -0500

    halfway

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,31 +2,49 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 
 public class SimpleClock extends JFrame {
-    
+
         Calendar calendar;
         SimpleDateFormat timeFormat;
         SimpleDateFormat dayFormat;
         SimpleDateFormat dateFormat;
-    
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
         String time;
         String day;
         String date;
-
         SimpleClock() {
+            JButton militaryClock = new JButton("Military Time");
+            militaryClock.setLayout(null);
+            militaryClock.setBounds(250,50,50,25);
+            militaryClock.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    boolean toggle = true;
+                    if(toggle) {
+                        timeFormat = new SimpleDateFormat("kk:mm:ss");
+                        toggle = false;
+                    }
+//                        if (!toggle) {
+//                        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+//                        toggle = true;
+//                    }
+                }
+            });
+
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
             this.setSize(350, 220);
             this.setResizable(false);
-    
+
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
             dayFormat=new SimpleDateFormat("EEEE");
             dateFormat=new SimpleDateFormat("dd MMMMM, yyyy");
@@ -37,27 +55,30 @@ public class SimpleClock extends JFrame {
             timeLabel.setOpaque(true);
             dayLabel=new JLabel();
             dayLabel.setFont(new Font("Ink Free",Font.BOLD,34));
-    
+
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
-    
+
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
+            this.add(militaryClock);
             this.setVisible(true);
-    
-            setTimer();
+
+
+
+            runClock();
+//            setTimer();
         }
-    
+
         public void setTimer() {
             while (true) {
                 time = timeFormat.format(Calendar.getInstance().getTime());
                 timeLabel.setText(time);
-    
+
                 day = dayFormat.format(Calendar.getInstance().getTime());
                 dayLabel.setText(day);
-    
+
                 date = dateFormat.format(Calendar.getInstance().getTime());
                 dateLabel.setText(date);
     
@@ -71,4 +92,17 @@ public class SimpleClock extends JFrame {
         public static void main(String[] args) {
             new SimpleClock();
         }
+        public void runClock() {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+
+            Thread clockThread = new Thread(this::runClock);
+            clockThread.start();
     }
+}
